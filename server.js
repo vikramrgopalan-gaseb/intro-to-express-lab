455656da426738cfe526c13174d7364bf0ab9149
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2025-11-13 18:55:42 -0700

    11/13 v2

--- server.js
+++ server.js
@@ -21,6 +21,21 @@ app.get('/greetings/:username', (req, res)=>{
     res.send(`Greetings, ${req.params.username}! I hope you have a nice day!`)
 })
 
+/* 2. Rolling the Dice
+Task: Set up a route to handle URLs following the pattern /roll/<number-parameter>.
+
+Examples: Matches routes like /roll/6 or /roll/20.
+
+Validation: If the parameter is not a number, respond with “You must specify a number.” For instance, /roll/potato should trigger this response.
+
+Functionality: If a valid number is provided, respond with a random whole number between 0 and the given number. For example, a request to /roll/16 might respond with “You rolled a 14.” */
+
+app.get('/roll/:number', (req, res)=>{
+    const dicenumber = req.query.dicenumber
+    if 
+        res.send(`You must specify a number.`)
+})
+
 //SERVER LISTENER
 app.listen(3000, ()=>{
     console.log('Listening on port 3000')
